3a0fe03bce5fc3163b0f807ab2b0b3b5cbaea045
Ernis <ernis.osmonkulov.05@gmail.com>
2024-09-21 18:56:44 +0600

    Update models.py
    ernis

--- laptop/laptop2/models.py
+++ laptop/laptop2/models.py
@@ -2,30 +2,28 @@ from django.db import models
 
 
 class Laptop(models.Model):
-    BRAND_CHOICES = [
-        ('mac os ', 'Mac OS '),
-        ('dell', 'Dell'),
-        ('hp', 'HP'),
-        ('lenovo', 'Lenovo'),
-        ('asus', 'Asus'),
-    ]
-
-    brand = models.CharField(max_length=88, choices=BRAND_CHOICES)
-    model = models.CharField(max_length=111)
-    processor = models.CharField(max_length=111)
-    ram_size = models.PositiveIntegerField()
-    storage_size = models.PositiveIntegerField()
+    BRAND_CHOICES = (
+        ('mac os ', 'mac OS'),
+        ('dell', 'dell'),
+        ('hp', 'hp'),
+        ('lenovo', 'lenovo'),
+        ('asus', 'asus'),
+    )
+    brand = models.CharField(max_length=32, choices=BRAND_CHOICES)
+    model = models.CharField(max_length=32)
+    processor = models.CharField(max_length=32)
+    ram_size = models.PositiveIntegerField(default=0)
+    storage_size = models.PositiveIntegerField(default=0)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
 
-    def __str__(self):
-        return f'{self.brand} {self.model}'
+    # def __str__(self):
+    #     return f'{self.brand} - {self.model}'
 
 
 class LaptopPhoto(models.Model):
-    laptop = models.ForeignKey(Laptop, related_name='photos', on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='laptop_photos/')
-    description = models.CharField(max_length=99, blank=True, null=True)
+    laptop = models.ForeignKey(Laptop, on_delete=models.CASCADE,related_name='photos')
+    image = models.ImageField(upload_to='img/')
 
-    def __str__(self):
-        return f'Photo of {self.laptop}'
+    # def __sr__(self):
+    #     return f'Photo of {self.laptop}'
