cc1d91bd11c0ff131e083b0756f132a28c5d7f0b
Ernis <ernis.osmonkulov.05@gmail.com>
2024-09-21 18:58:32 +0600

    Update admin.py
    sfsdf

--- laptop/laptop2/admin.py
+++ laptop/laptop2/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Laptop)
-admin.site.register(LaptopPhoto)
+
+class LaptopPhotosInline(admin.TabularInline):
+    model = LaptopPhoto
+    extra = 1
+
+
+class LaptopAdmin(admin.ModelAdmin):
+    inlines = [LaptopPhotosInline]
+
+
+admin.site.register(Laptop, LaptopAdmin)
